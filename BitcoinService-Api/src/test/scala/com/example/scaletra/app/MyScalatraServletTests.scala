9a0bc0957413859c072e076317a4525021513131
Ravichandra Reddy <30691682+maramravichandra@users.noreply.github.com>
2018-11-08 09:01:34 +0530

    Bitcoin: max price in bucket

--- BitcoinService-Api/src/test/scala/com/example/scaletra/app/MyScalatraServletTests.scala
+++ BitcoinService-Api/src/test/scala/com/example/scaletra/app/MyScalatraServletTests.scala
@@ -1,16 +1,32 @@
 package com.example.scaletra.app
 
+import com.bitcoin.beans.{BitcoinData, Price}
 import com.bitcoin.service.BitcoinServlet
+import com.bitcoin.util.BitcoinUtils
 import org.scalatra.test.scalatest._
+import org.scalatest.FunSuite
 
-class MyScalatraServletTests extends ScalatraFunSuite {
+class MyScalatraServletTests extends FunSuite {
 
-  addServlet(classOf[BitcoinServlet], "/*")
+   test("Test MaxPrice in Bitcoins Data") {
 
-  test("GET / on MyScalatraServlet should return status 200") {
-    get("/bitcoin") {
-      status should equal (200)
+    val bitcoin = new BitcoinData
+    val prices = new Array[Price](31)
+
+    for( i <- 0 to 30 ){
+      val date = BitcoinUtils.df.parse(s"2018-11-${i+1}")
+      val price = Price(s"${ i * 10 + i }", s"2018-11-${i+1}",date.getTime)
+      prices(i) = price
     }
+
+    bitcoin.data.prices = prices
+    bitcoin.updatePriceFields()
+
+    val result = BitcoinUtils.getMaxPriceInEachBucket(bitcoin,"2018-11-01","2018-11-30",7)
+
+    result.data.prices.foreach( println )
+
+    assert(result.errorMessage.trim.length == 0)
   }
 
 }
